70504576d17220ecdc676683bf6ffc691316dde8
Jose Miralles <josemiralles1991@gmail.com>
2021-03-08 00:30:27 -0500

    Get routes for commands work

---
+++ Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,51 @@
+﻿using Commander.Models;
+using System.Collections.Generic;
+
+namespace Commander.Data
+{
+
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+
+            List<Command> commands = new List<Command>
+            {
+                new Command {
+                    Id = 0,
+                    HowTo = "Boil an egg",
+                    Line = "Boil water",
+                    Platform = "Kettle and pan"
+                },
+                new Command {
+                    Id = 0,
+                    HowTo = "Make a cup of tea",
+                    Line = "Boil water",
+                    Platform = "Kettle and pot"
+                },
+                new Command {
+                    Id = 0,
+                    HowTo = "Slice bread",
+                    Line = "Get a knife",
+                    Platform = "Chopping board"
+                }
+            };
+
+            return commands;
+
+        }
+
+        public Command GetCommandById(int id)
+        {
+
+            return new Command {
+                    Id = 0,
+                    HowTo = "Boil an egg",
+                    Line = "Boil water",
+                    Platform = "Kettle and pan"
+                };
+
+        }
+    }
+
+}
